a18bfc01a9c3680b553716b7dda52d0a0df8c71a
subin_park <subin_park@woodigo.com>
2025-05-09 15:50:27 +0900

    add: ToolPanel에 undo/redo 버튼 및 단축키 추가

--- src/main/java/com/vector/editor/ToolPanel.java
+++ src/main/java/com/vector/editor/ToolPanel.java
@@ -7,6 +7,7 @@ import com.vector.editor.tools.RectangleTool;
 import com.vector.editor.tools.LineTool;
 import com.vector.editor.tools.TextTool;
 import com.vector.editor.utils.ImageLoader;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -79,6 +80,72 @@ public class ToolPanel extends JPanel {
 
         add(imageButton);
         add(Box.createVerticalStrut(5));
+    }
+
+    private void addUndoRedoButtons() {
+        // Undo button panel
+        JPanel undoPanel = new JPanel();
+        undoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
+        undoPanel.setMaximumSize(new Dimension(PANEL_WIDTH - 20, BUTTON_SIZE));
+        undoPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Undo button
+        JButton undoButton = new JButton("↩");
+        undoButton.setToolTipText("Undo (Ctrl+Z)");
+        undoButton.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
+        undoButton.addActionListener(e -> {
+            if (commandManager.canUndo()) {
+                commandManager.undo();
+                canvasPanel.repaint();
+            }
+        });
+
+        // Redo button
+        JButton redoButton = new JButton("↪");
+        redoButton.setToolTipText("Redo (Ctrl+Y)");
+        redoButton.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
+        redoButton.addActionListener(e -> {
+            if (commandManager.canRedo()) {
+                commandManager.redo();
+                canvasPanel.repaint();
+            }
+        });
+
+        undoPanel.add(undoButton);
+        undoPanel.add(redoButton);
+
+        add(undoPanel);
+        add(Box.createVerticalStrut(10));
+
+        // 키보드 단축키 추가 (Ctrl+Z, Ctrl+Y)
+        setupKeyboardShortcuts(undoButton, redoButton);
+    }
+
+    private void setupKeyboardShortcuts(JButton undoButton, JButton redoButton) {
+        // Undo 단축키 (Ctrl+Z)
+        InputMap inputMap = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = getActionMap();
+
+        KeyStroke undoKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_Z,
+            Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx());
+        inputMap.put(undoKeyStroke, "undo");
+        actionMap.put("undo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                undoButton.doClick();
+            }
+        });
+
+        // Redo 단축키 (Ctrl+Y)
+        KeyStroke redoKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_Y,
+            Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx());
+        inputMap.put(redoKeyStroke, "redo");
+        actionMap.put("redo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                redoButton.doClick();
+            }
+        });
     }
 
     public void setStrokeColor(Color strokeColor) {
